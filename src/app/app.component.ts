4ff99b5d17e41de94c9a3adcf0c10f1597fb16f4
Sourav-Halder <sourav@stockedge.com>
2025-07-22 11:57:41 +0530

    add seo implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ComingSoonComponent } from "./components/coming-soon/coming-soon.component";
+import { DOCUMENT } from '@angular/common';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,46 @@ import { ComingSoonComponent } from "./components/coming-soon/coming-soon.compon
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private doc: Document,
+    public titleService: Title, private metaService: Meta
+  ){
+    
+  }
   title = 'onestopformatter.dev';
+
+  ngOnInit(): void {
+    const script = this.renderer.createElement('script');
+    script.type = 'application/ld+json';
+    script.text = JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "WebApplication",
+      "name": "DevConvert",
+      "url": "https://devconvert.com",
+      "applicationCategory": "DeveloperApplication",
+      "operatingSystem": "All",
+      "description": "Convert JSON to Dart, JSON Viewer, XML to JSON, and more developer tools online for free."
+    });
+    this.renderer.appendChild(this.doc.head, script);
+
+    
+    this.titleService.setTitle('Coming Soon | DevConvert – Smart JSON & Data Tools');
+
+    this.metaService.addTags([
+      { name: 'description', content: 'We’re building DevConvert — a suite of smart, developer-focused tools like JSON to TypeScript, viewers, beautifiers, and converters. Be the first to explore it!' },
+      { name: 'robots', content: 'index, follow' },
+      { property: 'og:title', content: 'Coming Soon | DevConvert – Smart JSON & Data Tools' },
+      { property: 'og:description', content: 'We’re building DevConvert — a suite of smart, developer-focused tools like JSON to TypeScript, viewers, beautifiers, and converters. Be the first to explore it!' },
+      { property: 'og:image', content: 'https://yourdomain.com/assets/coming-soon-preview.png' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://yourdomain.com/' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:title', content: 'Coming Soon | DevConvert – Smart JSON & Data Tools' },
+      { name: 'twitter:description', content: 'We’re building DevConvert — a suite of smart, developer-focused tools like JSON to TypeScript, viewers, beautifiers, and converters. Be the first to explore it!' },
+      { name: 'twitter:image', content: 'https://yourdomain.com/assets/coming-soon-preview.png' }
+    ]);
+  }
+
 }
--- src/app/components/coming-soon/coming-soon.component.ts
+++ src/app/components/coming-soon/coming-soon.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component, } from '@angular/core';
 import { Analytics, logEvent } from '@angular/fire/analytics';
-import { Meta, Title } from '@angular/platform-browser';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -10,27 +9,13 @@ import { ToastrService } from 'ngx-toastr';
   styleUrl: './coming-soon.component.scss'
 })
 export class ComingSoonComponent {
-constructor(private toastr: ToastrService,private analytics:Analytics, private titleService: Title, private metaService: Meta) {}
+constructor(private toastr: ToastrService,private analytics:Analytics) {}
 
-ngOnInit(): void {
-  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-  //Add 'implements OnInit' to the class.
-   this.titleService.setTitle('Coming Soon | Onestopformatter.dev');
-
-this.metaService.addTags([
-  { property: 'og:title', content: 'Coming Soon | Onestopformatter.dev' },
-  { property: 'og:description', content: 'We are launching soon!' },
-  { property: 'og:url', content: 'https://onestopformatter.dev' },
-  { property: 'og:type', content: 'website' },
-  { name: 'twitter:card', content: 'summary_large_image' },
-  { name: 'twitter:title', content: 'Coming Soon | Onestopformatter.dev' },
-  { name: 'twitter:description', content: 'We are launching soon!' },
-]);
+ ngOnInit(): void {
 
   }
-logClick() {
-  console.log("logClick");
 
+logClick() {
   const checkboxes = document.querySelectorAll('input[name="role"]') as NodeListOf<HTMLInputElement>;
   const isAnyChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
 
